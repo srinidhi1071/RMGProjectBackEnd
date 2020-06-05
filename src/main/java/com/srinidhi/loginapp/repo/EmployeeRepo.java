7417116fec0c23a6e05aac8af6823d1d9d81f240
srinidhi1071 <srinidhi1071@gmail.com>
2020-06-05 20:34:32 +0530

    Updated

--- src/main/java/com/srinidhi/loginapp/repo/EmployeeRepo.java
+++ src/main/java/com/srinidhi/loginapp/repo/EmployeeRepo.java
@@ -1,11 +1,18 @@
 package com.srinidhi.loginapp.repo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.srinidhi.loginapp.model.Employee;
+import com.srinidhi.loginapp.model.User;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, String>{
 
+
+	public Employee findByEmpName(String empName);
+	Optional<Employee> findByUsername(String username);
 }
